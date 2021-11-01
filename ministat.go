733b43c7789883dbd06f9517f39425dc86a19e82
ondi <thy321@gmail.com>
2021-11-01 12:47:19 +0300

    minor fix

--- ministat.go
+++ ministat.go
@@ -55,7 +55,6 @@ type Stat_t struct {
 type Online interface {
 	MinistatOnline(r *http.Request, count int64) (*http.Request, error)
 	MinistatDuration(r *http.Request, status int, diff time.Duration)
-	MinistatEvict(key interface{})
 }
 
 type NoOnline_t struct{}
@@ -65,17 +64,11 @@ func (NoOnline_t) MinistatOnline(r *http.Request, count int64) (*http.Request, e
 }
 
 func (NoOnline_t) MinistatDuration(r *http.Request, status int, diff time.Duration) {}
-func (NoOnline_t) MinistatEvict(key interface{})                                    {}
 
-type Ministat_t struct {
-	mx            sync.Mutex
-	cc            *cache.Cache_t // key = ts.Truncate(self.truncate), value = *unique.Often_t
-	limit_backlog int
-	limit_items   int
-	truncate      time.Duration
-	begin         time.Time
-	online        Online
-	next          http.Handler
+type TooManyRequests struct{}
+
+func (TooManyRequests) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
 }
 
 type StatusResponseWriter struct {
@@ -95,20 +88,25 @@ func (self *StatusResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) 
 	return nil, nil, errors.New("not a http.Hijacker")
 }
 
-func New(limit_backlog int, limit_items int, truncate time.Duration, next http.Handler, online Online) (self *Ministat_t) {
-	self = &Ministat_t{
+type Storage_t struct {
+	mx            sync.Mutex
+	cc            *cache.Cache_t // key = ts.Truncate(self.truncate), value = *unique.Often_t
+	limit_backlog int
+	limit_items   int
+	truncate      time.Duration
+}
+
+func NewStorage(limit_backlog int, limit_items int, truncate time.Duration) (self *Storage_t) {
+	self = &Storage_t{
 		cc:            cache.New(),
 		limit_backlog: limit_backlog,
 		limit_items:   limit_items,
 		truncate:      truncate,
-		begin:         time.Now(),
-		online:        online,
-		next:          next,
 	}
 	return
 }
 
-func (self *Ministat_t) MetricBegin(name string, start time.Time, processed int) (counter *Counter_t) {
+func (self *Storage_t) MetricBegin(name string, start time.Time, processed int) (counter *Counter_t) {
 	self.mx.Lock()
 	it, _ := self.cc.CreateBack(
 		start.Truncate(self.truncate),
@@ -119,7 +117,6 @@ func (self *Ministat_t) MetricBegin(name string, start time.Time, processed int)
 	if self.cc.Size() > self.limit_backlog {
 		key := self.cc.Front().Key
 		self.cc.Remove(key)
-		self.online.MinistatEvict(key)
 	}
 	counter, _ = it.Value.(*unique.Often_t).Add(name, func() unique.Counter { return &Counter_t{} }).(*Counter_t)
 	counter.Online++
@@ -132,7 +129,7 @@ func (self *Ministat_t) MetricBegin(name string, start time.Time, processed int)
 	return
 }
 
-func (self *Ministat_t) MetricEnd(counter *Counter_t, diff time.Duration, status_code int) {
+func (self *Storage_t) MetricEnd(counter *Counter_t, diff time.Duration, status_code int) {
 	self.mx.Lock()
 	counter.Online--
 	counter.DurationSum += diff
@@ -152,31 +149,11 @@ func (self *Ministat_t) MetricEnd(counter *Counter_t, diff time.Duration, status
 	self.mx.Unlock()
 }
 
-func (self *Ministat_t) AddDuration(name string, processed int, start time.Time, diff time.Duration, status_code int) {
-	counter := self.MetricBegin(name, start, processed)
-	self.MetricEnd(counter, diff, status_code)
+func (self *Storage_t) AddDuration(name string, processed int, start time.Time, diff time.Duration, status_code int) {
+	self.MetricEnd(self.MetricBegin(name, start, processed), diff, status_code)
 }
 
-func (self *Ministat_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	writer := StatusResponseWriter{w, http.StatusOK}
-
-	counter := self.MetricBegin(GETURL(r), start, 1)
-
-	var diff time.Duration
-	req, err := self.online.MinistatOnline(r, counter.Online)
-	if err != nil {
-		http.Error(&writer, err.Error(), http.StatusTooManyRequests)
-	} else {
-		self.next.ServeHTTP(&writer, req)
-	}
-	diff = time.Since(start)
-	self.online.MinistatDuration(req, writer.status_code, diff)
-
-	self.MetricEnd(counter, diff, writer.status_code)
-}
-
-func (self *Ministat_t) List(order cache.MyLess) (res []Stat_t) {
+func (self *Storage_t) List(order cache.MyLess) (res []Stat_t) {
 	self.mx.Lock()
 	defer self.mx.Unlock()
 	for it := self.cc.Back(); it != self.cc.End(); it = it.Prev() {
@@ -196,6 +173,46 @@ func (self *Ministat_t) List(order cache.MyLess) (res []Stat_t) {
 		res = append(res, temp)
 	}
 	return
+}
+
+type Middleware_t struct {
+	storage *Storage_t
+	online  Online
+	ok      http.Handler
+	err     http.Handler
+}
+
+func NewMiddleware(limit_backlog int, limit_items int, truncate time.Duration, ok http.Handler, err http.Handler, online Online) (self *Middleware_t) {
+	self = &Middleware_t{
+		storage: NewStorage(limit_backlog, limit_items, truncate),
+		online:  online,
+		ok:      ok,
+		err:     err,
+	}
+	return
+}
+
+func (self *Middleware_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	writer := StatusResponseWriter{w, http.StatusOK}
+
+	counter := self.storage.MetricBegin(GETURL(r), start, 1)
+
+	var diff time.Duration
+	req, err := self.online.MinistatOnline(r, counter.Online)
+	if err != nil {
+		self.err.ServeHTTP(&writer, req)
+	} else {
+		self.ok.ServeHTTP(&writer, req)
+	}
+	diff = time.Since(start)
+	self.online.MinistatDuration(req, writer.status_code, diff)
+
+	self.storage.MetricEnd(counter, diff, writer.status_code)
+}
+
+func (self *Middleware_t) List(order cache.MyLess) (res []Stat_t) {
+	return self.storage.List(order)
 }
 
 type LessHits_t struct{}
