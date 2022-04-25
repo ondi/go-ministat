b730b36b6db9dd986d6822d3a12b365ff0e8e76b
ondi <thy321@gmail.com>
2022-04-25 17:55:27 +0300

    minor fix

---
+++ response_cors.go
@@ -0,0 +1,23 @@
+//
+//
+//
+
+package ministat
+
+import "net/http"
+
+type Cors_t struct {
+	Handler http.Handler
+}
+
+func (self Cors_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "*")
+		w.Header().Add("Access-Control-Allow-Headers", "*")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	self.Handler.ServeHTTP(w, r)
+}
