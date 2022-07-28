144626e2021fd90ddebe9b80cfc0949c8c386ff2
ondi <thy321@gmail.com>
2022-07-28 23:44:28 +0300

    minor fix

--- middleware.go
+++ middleware.go
@@ -16,25 +16,27 @@ func GetPageName(r *http.Request) (res string) {
 }
 
 type Middleware_t struct {
-	storage         *Storage_t
-	next            http.Handler
-	views           Views
-	page_name       func(*http.Request) string
-	online_limit    int64
-	online_duration time.Duration
+	storage           *Storage_t
+	next              http.Handler
+	views             Views
+	page_name         func(*http.Request) string
+	online_limit_hard int64
+	online_limit_soft int64
+	online_duration   time.Duration
 }
 
 type StatOption func(self *Middleware_t)
 
-func OnlineLimit(in int64) StatOption {
+func OnlineLimit(hard int64) StatOption {
 	return func(self *Middleware_t) {
-		self.online_limit = in
+		self.online_limit_hard = hard
 	}
 }
 
-func OnlineDuration(in time.Duration) StatOption {
+func OnlineDuration(soft int64, duration time.Duration) StatOption {
 	return func(self *Middleware_t) {
-		self.online_duration = in
+		self.online_limit_soft = soft
+		self.online_duration = duration
 	}
 }
 
@@ -46,11 +48,12 @@ func PageName(f func(*http.Request) string) StatOption {
 
 func NewMiddleware(storage *Storage_t, next http.Handler, views Views, opts ...StatOption) (self *Middleware_t) {
 	self = &Middleware_t{
-		storage:      storage,
-		next:         next,
-		views:        views,
-		page_name:    GetPageName,
-		online_limit: 1<<63 - 1,
+		storage:           storage,
+		next:              next,
+		views:             views,
+		page_name:         GetPageName,
+		online_limit_hard: 1<<63 - 1,
+		online_limit_soft: 1<<63 - 1,
 	}
 	for _, v := range opts {
 		v(self)
@@ -63,23 +66,23 @@ func (self *Middleware_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	page := self.page_name(r)
 	writer := ResponseWriter_t{ResponseWriter: w, status_code: http.StatusOK}
 
-	c, current := self.storage.MetricBegin(page, start)
+	p, c := self.storage.MetricBegin(page, start)
 
-	if current.Sampling > 0 {
+	if c.Sampling > 0 {
 		self.views.MinistatBefore(r.Context(), page)
 	}
-	if current.Online >= self.online_limit && current.DurationSum/current.DurationNum >= self.online_duration {
+	if c.Online >= self.online_limit_hard || c.Online >= self.online_limit_soft && c.DurationSum/c.DurationNum >= self.online_duration {
 		log.WarnCtx(r.Context(), "TOO MANY REQUESTS: %v", page)
 		http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
 	} else {
 		self.next.ServeHTTP(&writer, r)
 	}
-	if current.Sampling > 0 {
+	if c.Sampling > 0 {
 		self.views.MinistatAfter(r.Context(), page)
 	}
 
 	diff := time.Since(start)
-	if self.storage.MetricEnd(c, diff, 1, writer.status_code).Sampling > 0 {
+	if self.storage.MetricEnd(p, diff, 1, writer.status_code).Sampling > 0 {
 		self.views.MinistatDuration(r.Context(), page, diff, 1, writer.status_code)
 	}
 }
