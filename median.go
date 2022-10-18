11e82e198ce5ab9091f15287af1a6ae5728ac067
ondi <thy321@gmail.com>
2022-10-18 21:50:46 +0300

    debug

--- median.go
+++ median.go
@@ -5,8 +5,6 @@
 package ministat
 
 import (
-	"fmt"
-	"os"
 	"sync"
 
 	"github.com/ondi/go-cache"
@@ -40,21 +38,18 @@ func (self *Median_t[Value_t]) Add(value Value_t, cmp ValueCmp_t[Value_t]) (res 
 		self.seq = 0
 	}
 	var prev_less_than_median bool
-	fmt.Fprintf(os.Stderr, "###########: value=%v, median=%v, left=%v, right=%v, values=%v\n", value, self.median.Value, self.left, self.right, self.Values())
 	it, ok := self.cc.CreateFront(self.seq, func() Value_t{return value})
 	if !ok {
 		// тут нужно знать из какой половины списка старый элемент
-		// чтобы понимать надо ли менять число элементов слева и справа медианы или оставить как есть
+		// чтобы скорректировать число элементов слева и справа медианы или оставить как есть
 		if cmp(it.Value, self.median.Value) <= 0 {
 			prev_less_than_median = true
 		}
-		fmt.Fprintf(os.Stderr, "OVERWRITE VALUE: old=%v, new=%v, prev_less=%v, median=%v, left=%v, right=%v, values=%v\n", it.Value, value, prev_less_than_median, self.median.Value, self.left, self.right, self.Values())
 		if it == self.median {
 			self.median = self.median.Next()
 			prev_less_than_median = true
 			self.left++
 			self.right--
-			fmt.Fprintf(os.Stderr, "OVERWRITE MEDIAN: old=%v, new=%v, prev_less=%v, median=%v, left=%v, right=%v, values=%v\n", it.Value, value, prev_less_than_median, self.median.Value, self.left, self.right, self.Values())
 		}
 		it.Value = value
 	}
@@ -89,13 +84,6 @@ func (self *Median_t[Value_t]) Range(f func(key int64, value Value_t) bool) {
 	}
 }
 
-func (self *Median_t[Value_t]) Values() (res []Value_t) {
-	for it := self.cc.Front(); it != self.cc.End(); it = it.Next() {
-		res = append(res, it.Value)
-	}
-	return
-}
-
 func (self *Median_t[Value_t]) RealMedian() (it *cache.Value_t[int64, Value_t]) {
 	half := self.cc.Size() / 2
 	for it = self.cc.Front(); half > 0; it = it.Next() {
@@ -105,9 +93,6 @@ func (self *Median_t[Value_t]) RealMedian() (it *cache.Value_t[int64, Value_t]) 
 }
 
 func (self *Median_t[Value_t]) set_median(it *cache.Value_t[int64, Value_t], median_passed bool, inserted bool, prev_less_than_median bool) {
-	
-	fmt.Fprintf(os.Stderr, "SET MEDIAN BEGIN: inserted=%v, passed=%v, before=%v, left=%v, right=%v, median=%v, values=%v\n", inserted, median_passed, prev_less_than_median, self.left, self.right, self.median.Value, self.Values())
-	
 	if median_passed {
 		if inserted {
 			self.right++
@@ -125,27 +110,22 @@ func (self *Median_t[Value_t]) set_median(it *cache.Value_t[int64, Value_t], med
 	} else {
 		self.median = it
 	}
-
-	fmt.Fprintf(os.Stderr, "SET MEDIAN   END: inserted=%v, passed=%v, before=%v, left=%v, right=%v, median=%v, values=%v\n", inserted, median_passed, prev_less_than_median, self.left, self.right, self.median.Value, self.Values())
-
 	if self.right < self.left - 1 {
 		self.median = self.median.Prev()
 		self.left--
 		self.right++
-		fmt.Fprintf(os.Stderr, "MEDIAN MOVE PREV: left=%v, right=%v, median=%v, values=%v\n", self.left, self.right, self.median.Value, self.Values())
 	} else if self.left < self.right - 1 {
 		self.median = self.median.Next()
 		self.left++
 		self.right--
-		fmt.Fprintf(os.Stderr, "MEDIAN MOVE NEXT: left=%v, right=%v, median=%v, values=%v\n", self.left, self.right, self.median.Value, self.Values())
 	}
-	it = self.cc.Front()
-	for i := int64(0); i < self.left; i++ {
-		it = it.Next()
-	}
-	if it.Key != self.median.Key {
-		panic(fmt.Sprintf("MEDIAN CHECK: left=%v, right=%v, check=%v, median=%v", self.left, self.right, it, self.median))
-	}
+	// it = self.cc.Front()
+	// for i := int64(0); i < self.left; i++ {
+	// 	it = it.Next()
+	// }
+	// if it.Key != self.median.Key {
+	// 	panic(fmt.Sprintf("MEDIAN CHECK: left=%v, right=%v, check=%v, median=%v", self.left, self.right, it, self.median))
+	// }
 }
 
 func (self *Median_t[Value_t]) insert_value(it *cache.Value_t[int64, Value_t], cmp ValueCmp_t[Value_t]) (median_passed bool) {
