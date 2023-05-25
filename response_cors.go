2f87bf5c1c0bb91614be065b7131ded60ee475f3
ondi <thy321@gmail.com>
2023-05-25 20:51:49 +0300

    minor fix

--- response_cors.go
+++ response_cors.go
@@ -8,26 +8,23 @@ import "net/http"
 
 type cors_t struct {
 	handler http.Handler
-	host    string
 }
 
-func NewCors(handler http.Handler, host string) *cors_t {
+func NewCors(handler http.Handler) *cors_t {
 	return &cors_t{
 		handler: handler,
-		host:    host,
 	}
 }
 
 func (self *cors_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	origin := r.Header.Get("Origin")
+	w.Header().Add("Access-Control-Allow-Origin", origin)
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
 	if r.Method == "OPTIONS" {
-		w.Header().Add("Access-Control-Allow-Origin", self.host)
 		w.Header().Add("Access-Control-Allow-Methods", "*")
 		w.Header().Add("Access-Control-Allow-Headers", "*")
-		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
-	w.Header().Add("Access-Control-Allow-Origin", self.host)
-	w.Header().Add("Access-Control-Allow-Credentials", "true")
 	self.handler.ServeHTTP(w, r)
 }
