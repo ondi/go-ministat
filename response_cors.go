f9fa2a9464dbccaaab84da005a090a83c51a8d9f
ondi <thy321@gmail.com>
2023-05-25 13:17:24 +0300

    minor fix

--- response_cors.go
+++ response_cors.go
@@ -15,9 +15,11 @@ func (self *Cors_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Access-Control-Allow-Methods", "*")
 		w.Header().Add("Access-Control-Allow-Headers", "*")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
 	self.Handler.ServeHTTP(w, r)
 }
