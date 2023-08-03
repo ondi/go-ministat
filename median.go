644add4546d7d8b32aea8f780c1b4cd5ed5c7e0e
ondi <thy321@gmail.com>
2023-08-03 19:10:25 +0300

    minor fix

--- median.go
+++ median.go
@@ -12,7 +12,8 @@ import (
 
 type Number interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64
 }
 
 type Mapped_t[T Number] struct {
