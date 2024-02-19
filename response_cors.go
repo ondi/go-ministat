02860acea24c47d843c845d839e80d3f77ab4a54
ondi <thy321@gmail.com>
2024-02-19 20:34:59 +0300

    minor fix

--- response_cors.go
+++ response_cors.go
@@ -17,15 +17,13 @@ func NewCors(handler http.Handler) *cors_t {
 }
 
 func (self *cors_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodOptions {
-		origin := r.Header.Get("Origin")
-		if len(origin) == 0 {
-			origin = "*"
-		}
+	if origin := r.Header.Get("Origin"); len(origin) > 0 {
 		w.Header().Add("Access-Control-Allow-Origin", origin)
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.Header().Add("Access-Control-Allow-Methods", "*")
 		w.Header().Add("Access-Control-Allow-Headers", "*")
+	}
+	if r.Method == http.MethodOptions {
 		return
 	}
 	self.handler.ServeHTTP(w, r)
