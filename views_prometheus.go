f1af72a223c9710ab21645daaf44b40b4c4b3621
ondi <thy321@gmail.com>
2024-08-28 23:48:16 +0300

    minor fix

--- views_prometheus.go
+++ views_prometheus.go
@@ -3,9 +3,9 @@
 // sum(http_page_load{app="$app_name",type="rps"}) by (page)
 // sum(http_page_load{app="$app_name",type="pending"}) by (page)
 // sum(http_page_load{app="$app_name",type="size"}) by (page)
-// max(http_page_latency{app="$app_name",type="max"}) by (page)
-// max(http_page_latency{app="$app_name",type="avg"}) by (page)
-// max(http_page_latency{app="$app_name",type="med"}) by (page)
+// max(http_page_load{app="$app_name",type="max"}) by (page)
+// max(http_page_load{app="$app_name",type="avg"}) by (page)
+// max(http_page_load{app="$app_name",type="med"}) by (page)
 // sum(rate(http_page_processed{app="$app_name"})[1m]) by (page,status)
 // sum(rate(http_page_error{app="$app_name"}[1m])) by (page,error)
 //
@@ -19,8 +19,7 @@ import (
 )
 
 type Prometheus_t struct {
-	Load      *prometheus.GaugeVec // pending, hits, rps, latency size
-	Latency   *prometheus.GaugeVec // med, max, avg
+	Load      *prometheus.GaugeVec // rps, hits, pending, latency, size
 	Processed *prometheus.GaugeVec // status
 	Error     *prometheus.GaugeVec // errors
 }
@@ -30,14 +29,10 @@ type Prometheus_t struct {
 func NewPrometheusViews(prefix string) (views Views[Page_t], err error) {
 	self := &Prometheus_t{
 		Load:      prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: prefix + "load"}, []string{"type", "page", "entry"}),
-		Latency:   prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: prefix + "latency"}, []string{"type", "page", "entry"}),
 		Processed: prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: prefix + "processed"}, []string{"page", "entry", "status"}),
 		Error:     prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: prefix + "error"}, []string{"page", "entry", "error"}),
 	}
 	if err = prometheus.Register(self.Load); err != nil {
-		return
-	}
-	if err = prometheus.Register(self.Latency); err != nil {
 		return
 	}
 	if err = prometheus.Register(self.Processed); err != nil {
@@ -50,7 +45,7 @@ func NewPrometheusViews(prefix string) (views Views[Page_t], err error) {
 }
 
 func (self *Prometheus_t) HitCurrent(ctx context.Context, page Page_t, g []Gauge_t) (err error) {
-	var _load, _latency, _processed, _error prometheus.Gauge
+	var _load, _processed, _error prometheus.Gauge
 	for _, v := range g {
 		switch v.Type {
 		case "processed":
@@ -65,12 +60,6 @@ func (self *Prometheus_t) HitCurrent(ctx context.Context, page Page_t, g []Gauge
 				continue
 			}
 			_error.Set(float64(v.Value))
-		case "med", "max", "avg":
-			_latency, err = self.Latency.GetMetricWith(prometheus.Labels{"type": v.Type, "page": page.Name, "entry": page.Entry})
-			if err != nil {
-				continue
-			}
-			_latency.Set(float64(v.Value))
 		default:
 			_load, err = self.Load.GetMetricWith(prometheus.Labels{"type": v.Type, "page": page.Name, "entry": page.Entry})
 			if err != nil {
