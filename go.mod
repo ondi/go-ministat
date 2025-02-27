0721b656b803fdcc47996552998fd1c210880020
ondi <thy321@gmail.com>
2025-02-27 17:27:16 +0300

    minor fix

--- go.mod
+++ go.mod
@@ -6,19 +6,19 @@ require (
 	github.com/ondi/go-cache v0.0.0-20230425151132-e34113a7989a
 	github.com/ondi/go-tst v0.0.0-20241202144437-eda7f5ee386d
 	github.com/ondi/go-unique v0.0.0-20241101150602-6e5d28af9297
-	github.com/prometheus/client_golang v1.20.5
+	github.com/prometheus/client_golang v1.21.0
 	gotest.tools v2.2.0+incompatible
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
-	github.com/prometheus/common v0.60.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
-	golang.org/x/sys v0.27.0 // indirect
-	google.golang.org/protobuf v1.35.2 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 )
--- response_cors.go
+++ response_cors.go
@@ -4,19 +4,23 @@
 
 package ministat
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+	"time"
+)
 
-type cors_t struct {
+type cors_middleware_t struct {
 	handler http.Handler
 }
 
-func NewCors(handler http.Handler) *cors_t {
-	return &cors_t{
+func NewCors(handler http.Handler) *cors_middleware_t {
+	return &cors_middleware_t{
 		handler: handler,
 	}
 }
 
-func (self *cors_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (self *cors_middleware_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if origin := r.Header.Get("Origin"); len(origin) > 0 {
 		w.Header().Add("Access-Control-Allow-Origin", origin)
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
@@ -27,4 +31,22 @@ func (self *cors_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	self.handler.ServeHTTP(w, r)
+}
+
+type timeout_middleware_t struct {
+	handler http.Handler
+	timeout time.Duration
+}
+
+func NewCtxTimeout(handler http.Handler, timeout time.Duration) *timeout_middleware_t {
+	return &timeout_middleware_t{
+		handler: handler,
+		timeout: timeout,
+	}
+}
+
+func (self *timeout_middleware_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), self.timeout)
+	defer cancel()
+	self.handler.ServeHTTP(w, r.WithContext(ctx))
 }
