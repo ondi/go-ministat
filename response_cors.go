ae8eabd4e19a68e839b28fb658717005c122193b
ondi <thy321@gmail.com>
2025-02-28 12:27:31 +0300

    minor fix

--- response_cors.go
+++ response_cors.go
@@ -9,6 +9,8 @@ import (
 	"errors"
 	"net/http"
 	"time"
+
+	"github.com/ondi/go-tst"
 )
 
 type cors_middleware_t struct {
@@ -35,21 +37,28 @@ func (self *cors_middleware_t) ServeHTTP(w http.ResponseWriter, r *http.Request)
 }
 
 type timeout_middleware_t struct {
+	routes  *tst.Tree3_t[time.Duration]
 	handler http.Handler
-	timeout time.Duration
 	cause   error
 }
 
-func NewCtxTimeout(handler http.Handler, timeout time.Duration, cause string) *timeout_middleware_t {
-	return &timeout_middleware_t{
+func NewCtxTimeout(handler http.Handler, cause string, routes map[string]time.Duration) (self *timeout_middleware_t) {
+	self = &timeout_middleware_t{
 		handler: handler,
-		timeout: timeout,
 		cause:   errors.New(cause),
+		routes:  tst.NewTree3[time.Duration](),
 	}
+	for k, v := range routes {
+		self.routes.Add(k, v)
+	}
+	return
 }
 
 func (self *timeout_middleware_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeoutCause(r.Context(), self.timeout, self.cause)
-	defer cancel()
-	self.handler.ServeHTTP(w, r.WithContext(ctx))
+	if value, _, found := self.routes.Search(r.URL.Path); found > 0 {
+		ctx, cancel := context.WithTimeoutCause(r.Context(), value, self.cause)
+		defer cancel()
+		r = r.WithContext(ctx)
+	}
+	self.handler.ServeHTTP(w, r)
 }
